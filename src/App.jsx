3ceece0f0ba31fbafa1064142f1cfed5236c84fe
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-12 12:54:00 +0530

    configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
