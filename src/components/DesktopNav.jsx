5a2d17020def267b682a656f0523b5e642285c17
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-12 15:11:11 +0530

    update DekstopNav

--- src/components/DesktopNav.jsx
+++ src/components/DesktopNav.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DesktopNav = () => {
-  return <div>DesktopNav</div>;
+  return (
+    <nav className="hidden lg:flex gap-x-12 font-semibold">
+      <Link to="/" className="text-gray-400 hover:text-gray-800 transition">
+        Home
+      </Link>
+      <Link
+        to="/about"
+        className="text-gray-400 hover:text-gray-800 transition"
+      >
+        About
+      </Link>
+      <Link
+        to="/projects"
+        className="text-gray-400 hover:text-gray-800 transition"
+      >
+        Projects
+      </Link>
+      <Link
+        to="/contact"
+        className="text-gray-400 hover:text-gray-800 transition"
+      >
+        Contact
+      </Link>
+    </nav>
+  );
 };
 
 export default DesktopNav;
