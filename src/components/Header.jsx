1cf532799ddc0326255f81ffccec3e6da369e044
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-13 12:57:02 +0530

    update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
+import Logo from "../assets/Logo.png";
 
 const Header = () => {
   return (
     <header className="w-full h-[5rem] lg:h-[7rem] fixed px-9 lg:px-24 flex items-center">
       <div className="w-full flex lg:flex-row lg:items-center justify-between">
         {/* logo image */}
-        <Link to="/">
-          {/* <img src="" alt="" /> */}
-          <h1 className="text-2xl font-semibold tracking-wide">Mohit.dev</h1>
+        <Link to="/" className="flex gap-2 items-center">
+          <img src={Logo} alt="" className="w-7 lg:w-10 lg:h-10 h-7" />
+          <h1 className="font-serif text-2xl font-semibold">Mohit</h1>
         </Link>
 
         {/* Desktop Navigation */}
