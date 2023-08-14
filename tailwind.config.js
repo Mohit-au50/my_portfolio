7b8f5bfd12a8920c89e09b07400d62d37de9ac8f
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-14 14:47:18 +0530

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        blob: "42% 56% 72% 28% / 42% 42% 56% 48%",
+      },
+      keyframes: {
+        morph: {
+          "0% 100%": {
+            "border-radius": "42% 56% 72% 28% / 42% 42% 56% 48%",
+          },
+          "33%": {
+            "border-radius": "72% 28% 48% 48% / 28% 28% 72% 72%",
+          },
+          "66%": {
+            "border-radius": "100% 56% 56% 100% / 100% 100% 56% 56%",
+          },
+        },
+
+        scroll: {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-50.5%)",
+          },
+        },
+      },
+      animation: {
+        morph: "morph 3.75s linear infinite",
+        scroll: "scroll 27s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
