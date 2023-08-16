03e353aa95de55369390f5079e2448c19ff700f4
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-16 19:59:59 +0530

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ import Logo from "../assets/Logo.png";
 
 const Header = () => {
   return (
-    <header className="w-full h-[5rem] lg:h-[7rem] fixed px-9 lg:px-24 flex items-center">
-      <div className="w-full flex lg:flex-row lg:items-center justify-between">
+    <header className="z-10 w-full h-[5rem] lg:h-[7rem] fixed px-9 lg:px-24 flex items-center">
+      <div className="w-full flex lg:flex-row items-center justify-between">
         {/* logo image */}
         <Link to="/" className="flex gap-2 items-center">
           <img src={Logo} alt="" className="w-7 lg:w-10 lg:h-10 h-7" />
-          <h1 className="font-serif text-2xl font-semibold">Mohit</h1>
+          <span className="font-semibold text-lg">Mohit</span>
         </Link>
 
         {/* Desktop Navigation */}
